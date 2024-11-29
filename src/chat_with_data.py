e3d75091be67f52017f53ecbdec21c8fa3fad28c
Rehanga Gamage <rehanga@live.com>
2024-11-29 13:56:05 +0530

    Chat with textbook page now only gives the useful sources (and not just the top k)

--- src/chat_with_data.py
+++ src/chat_with_data.py
@@ -1,8 +1,13 @@
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field
 
 from src.connectors import Connectors
+
+class ResultWithSourcesUsed(BaseModel):
+    answer: str = Field(description='Your answer.')
+    context_pieces_used: list[int] = Field(
+        description="""An array of the numbers of context pieces used in order of descending importance. For example if you were given
+         3 pieces of context and piece 3 was the most important, followed by piece 2 and piece 1 was useless, array must be [3,2] """
+    )
 
 
 def chat_with_data(query: str) -> tuple[str,list[dict]]:
@@ -15,39 +20,37 @@ def chat_with_data(query: str) -> tuple[str,list[dict]]:
         tuple[str,list[dict]]: answer, sources
     """
     llm = Connectors.get_llm_client()
+    structured_llm = llm.with_structured_output(ResultWithSourcesUsed)
     vectorstore = Connectors.get_vectorstore_client()
     retriever = vectorstore.as_retriever()
 
-    system_prompt = (
-        "You are an assistant for question-answering tasks. "
-        "Use ONLY the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Keep the "
-        "answer concise."
-        "\n\n"
-        "{context}"
-    )
+    context_docs = retriever.invoke(query)
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", system_prompt),
-            ("human", "{input}"),
-        ]
-    )
+    prompt_string = f"""
+    You are an assistant for question-answering tasks. 
+    Use ONLY the following pieces of retrieved context to answer
+    the question. If you don't know the answer, say that you don't know. 
+    Keep the answer concise. The retrieved pieces of context are numbered.
+    Question:
 
-    question_answer_chain = create_stuff_documents_chain(llm, prompt)
-    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    """
+    prompt_string += query + '\n'
+    prompt_string += 'CONTEXT:  \n'
 
-    result = rag_chain.invoke({"input": query})
+    for i,doc in enumerate(context_docs):
+        prompt_string += f"{i + 1}. {doc.page_content}\n"
+    
 
-    answer = result['answer']
+    result = structured_llm.invoke(prompt_string)
+    print(result)
     sources = []
 
-    context_documents = result['context']
-    for document in context_documents:
+    for n in result.context_pieces_used:
+        index = n-1
+        document = context_docs[index]
         sources.append(document.metadata)
 
-    return (answer,sources)
+    return (result.answer,sources)
 
 
 
