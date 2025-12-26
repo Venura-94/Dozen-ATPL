4c4177ead7821e2a33f0736e1a717134195aaaf5
Rehanga Gamage <rehanga@live.com>
2025-12-26 15:16:01 +0530

    Users can now click on a source (in the chat with textbook page) to view a markdown popover of that section of the textbook.

--- pages/chat_with_textbook.py
+++ pages/chat_with_textbook.py
@@ -28,8 +28,14 @@ if "messages_for_memory" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message['role'] != 'source':
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    else:
+        source = message["content"]
+        with st.chat_message("assistant"):
+            with st.popover(f"**Chapter {source['chapter']} -> {source['subchapter']}**"):
+                st.markdown(source['markdown'])
 
 # React to user input
 if prompt := st.chat_input("Enter question..."): # := operator to assign the user's input to the prompt variable and checked if it's not None in the same line.
@@ -55,10 +61,11 @@ if prompt := st.chat_input("Enter question..."): # := operator to assign the use
 
     sources_string = 'Sources:  \n  \n'
     for source in sources:
-        sources_string += f"Chapter {source['chapter']} -> {source['subchapter']}" + '  \n' # streamlit requires 2 whitespaces in front of the new line for it to work
-    with st.chat_message("assistant"):
-        st.write_stream(typewriter(sources_string))
-    st.session_state.messages.append({"role": "assistant", "content": sources_string})
+        source_string = f"Chapter {source['chapter']} -> {source['subchapter']}" + '  \n' # streamlit requires 2 whitespaces in front of the new line for it to work
+        with st.chat_message("assistant"):
+            with st.popover(f"**Chapter {source['chapter']} -> {source['subchapter']}**"):
+                st.markdown(source['markdown'])
+        st.session_state.messages.append({"role": "source", "content": source})
 
     
     # cap to last 18 messages
